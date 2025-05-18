2c1ea61b57f28ac2c4b04b30c93ce6b5814ed77a
Yerlan Nurlibayev <106547481+Aiknn@users.noreply.github.com>
2025-05-18 18:10:20 +0000

    readonly for non-get methods

---
+++ backend/src/main/java/com/luv2code/config/MyDataRestConfig.java
@@ -0,0 +1,33 @@
+package com.luv2code.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer; 
+
+
+import com.luv2code.entity.Book;
+
+@Configuration
+public class MyDataRestConfig implements RepositoryRestConfigurer{
+    private String theAllowedOrigins = "http://localhost:3000";
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors){
+        HttpMethod[] theUnsupportedActions = {HttpMethod.POST, HttpMethod.PATCH, HttpMethod.DELETE, HttpMethod.PUT};
+        config.exposeIdsFor(Book.class);
+        disableHttpMethods(Book.class, config, theUnsupportedActions);
+        cors.addMapping(config.getBasePath() + "/**")
+        .allowedOrigins(theAllowedOrigins);
+    }
+
+    private void disableHttpMethods(Class<?> theClass, RepositoryRestConfiguration config,
+            HttpMethod[] theUnsupportedActions) {
+        config.getExposureConfiguration().forDomainType(theClass)
+        .withItemExposure((metdata, HttpMethods) ->
+        HttpMethods.disable(theUnsupportedActions))
+        .withCollectionExposure((metdata, HttpMethods) ->
+        HttpMethods.disable(theUnsupportedActions));
+    }
+}
