411a1952b1f1403948bf8613b706dc9de2130feb
Yerlan Nurlibayev <106547481+Aiknn@users.noreply.github.com>
2025-06-18 07:13:36 +0000

    added search functional

--- backend/src/main/java/com/luv2code/dao/BookRepository.java
+++ backend/src/main/java/com/luv2code/dao/BookRepository.java
@@ -1,9 +1,14 @@
 package com.luv2code.dao;
 
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.luv2code.entity.Book;
 
 public interface BookRepository extends JpaRepository<Book, Long>{
 
+    Page<Book> findByTitleContaining(@RequestParam("title") String title, Pageable pageable);
 }
