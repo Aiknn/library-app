0a6b84ff1a3d4f98d9701c387ed74769b0206416
Yerlan Nurlibayev <106547481+Aiknn@users.noreply.github.com>
2025-07-04 09:31:46 +0000

    added backend for review

--- backend/src/main/java/com/luv2code/config/MyDataRestConfig.java
+++ backend/src/main/java/com/luv2code/config/MyDataRestConfig.java
@@ -8,6 +8,7 @@ import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 
 
 import com.luv2code.entity.Book;
+import com.luv2code.entity.Review;
 
 @Configuration
 public class MyDataRestConfig implements RepositoryRestConfigurer{
@@ -17,7 +18,9 @@ public class MyDataRestConfig implements RepositoryRestConfigurer{
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors){
         HttpMethod[] theUnsupportedActions = {HttpMethod.POST, HttpMethod.PATCH, HttpMethod.DELETE, HttpMethod.PUT};
         config.exposeIdsFor(Book.class);
+        config.exposeIdsFor(Review.class);
         disableHttpMethods(Book.class, config, theUnsupportedActions);
+        disableHttpMethods(Review.class, config, theUnsupportedActions);
         cors.addMapping(config.getBasePath() + "/**")
         .allowedOrigins(theAllowedOrigins);
     }
